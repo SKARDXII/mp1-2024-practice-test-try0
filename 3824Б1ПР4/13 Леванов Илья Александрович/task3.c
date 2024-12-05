6af0af2665aae6c9b52e17ec0db0de562f929262
SKARDXII <124432259+SKARDXII@users.noreply.github.com>
2024-12-05 09:56:50 +0300

    Update task3.c

--- 3824Б1ПР4/13 Леванов Илья Александрович/task3.c
+++ 3824Б1ПР4/13 Леванов Илья Александрович/task3.c
@@ -1,5 +1,18 @@
-// Задача 3. Найти максимум из вешественных чисел a и b
+/* // Задача 3. Найти максимум из вешественных чисел a и b
 double max_of_two_numbers(double a, double b)
 {
   return 0.0;
+} */
+
+
+// ================================
+
+#include <cmath>
+
+double max_of_two_numbers(double a, double b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
 }
